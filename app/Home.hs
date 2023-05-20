c12e025a5be68d3ae2936fab06ea97d1bcd10377
givingdonation <gitlab@iscienceworld.com>
2023-05-20 12:57:09 -0700

    added mathjax and holiday css

--- app/Home.hs
+++ app/Home.hs
@@ -2,13 +2,20 @@
 module Home (render) where
 
 import Text.Blaze.Html5 as H
-import Text.Blaze.Html5.Attributes
+import Text.Blaze.Html5.Attributes as A
 
 render = do
-  html $ do
-    H.head $ link ! rel "stylesheet" ! href "https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css"
+  html ! class_ "holiday-css-dark" $ do
+    H.head $ do 
+      meta ! charset "UTF-8"
+      meta ! name "viewport" ! content "width=device-width, initial-scale=1.0"
+      script ! src "https://polyfill.io/v3/polyfill.min.js?features=es6" $ ""
+      script ! A.id "MathJax-script" ! async "true" ! src "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js" $ ""
+      link ! rel "stylesheet" ! href "https://cdn.jsdelivr.net/npm/holiday.css@0.11.0"
+      H.title "Lambda Math"
     body $ do
-        h1 "Lambdamath"
-        ul $ do
-            li "Practice"
+        nav $ ul $ do
+            li $ a "Practice"
             li "Learn"
+            li "$$\\lambda$$"
+        
